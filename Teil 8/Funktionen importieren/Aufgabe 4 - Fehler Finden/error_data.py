9845d46989a1d5377f562cef90b848f0eb61875f
Leo <97408405+Ramzkeks@users.noreply.github.com>
2025-01-22 10:51:39 +0100

    Create error_data.py

---
+++ Teil 8/Funktionen importieren/Aufgabe 4 - Fehler Finden/error_data.py
@@ -0,0 +1,6 @@
+# def greet(name) - There is an error here. 
+# The definition of the greet(name) function does not contain a colon (:) at the end of the line. 
+# This will cause a SyntaxError.
+# So, we can fix this error by adding a colon (:) at the end of the line.
+def greet(name):
+    print(f"Hallo, {name}!")
