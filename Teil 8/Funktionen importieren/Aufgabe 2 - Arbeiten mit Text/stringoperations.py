c8e1cef0023c1034a7c13d7122a2a635433e300b
Leo <97408405+Ramzkeks@users.noreply.github.com>
2025-01-22 10:48:44 +0100

    Delete Teil 8/Funktionen importieren/Aufgabe 2 - Arbeiten mit Text directory

--- Teil 8/Funktionen importieren/Aufgabe 2 - Arbeiten mit Text/stringoperations.py
+++
@@ -1,7 +0,0 @@
-def count_words(text):
-    """Counts the number of words in a string."""
-    return len(text.split())
-
-def replace_word(text, old_word, new_word):
-    """Replaces a word in a string with another word."""
-    return text.replace(old_word, new_word)
