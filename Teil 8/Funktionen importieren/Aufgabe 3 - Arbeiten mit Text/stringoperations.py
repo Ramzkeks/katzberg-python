44f530930c7b015a231d1e636c6b61313b0ea00f
Leo <97408405+Ramzkeks@users.noreply.github.com>
2025-01-22 10:49:25 +0100

    Create stringoperations.py

---
+++ Teil 8/Funktionen importieren/Aufgabe 3 - Arbeiten mit Text/stringoperations.py
@@ -0,0 +1,7 @@
+def count_words(text):
+    """Counts the number of words in a string."""
+    return len(text.split())
+
+def replace_word(text, old_word, new_word):
+    """Replaces a word in a string with another word."""
+    return text.replace(old_word, new_word)
